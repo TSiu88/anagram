851022161918c5467ad33c2ee863c8019ac9ab46
Jack Dunning and Tiffany Siu <tiffany@>
2020-03-03 15:42:29 -0800

    Add tests and partial match, need to fix errors

--- Anagram.Tests/ModelTests/WordsTests.cs
+++ Anagram.Tests/ModelTests/WordsTests.cs
@@ -3,19 +3,57 @@ using AnagramList.Models;
 using System;
 using System.Collections.Generic;
 
-namespace AnagramList.Models
+namespace AnagramList.Tests
 {
   [TestClass]
-  public class WordsTests
+  public class WordsTests : IDisposable
   {
-    
+    public void Dispose()
+    {
+      Words.ClearAll();
+    }
     [TestMethod]
     public void AddToList_AddsItemToInputList_InputList()
     {
       Words myWords = new Words("ball", "bite bool labbo llab");
-      myWords.AddToList();
       List <string> output = new List<string> {"bite", "bool", "labbo", "llab"};
-      CollectionAssert.AreEquivalent(output, myWords.InputList);
+      CollectionAssert.AreEquivalent(output, Words.InputList);
+    }
+    
+    [TestMethod]
+    public void AnagramMatch_ReturnsAnagrams_ListOfAnagrams()
+    {
+      Words myWords = new Words("ball", "bite bool labbo llab");
+      List <string> output = new List<string> {"llab"};
+      List<string> test = myWords.AnagramMatch();
+      for(int i=0; i< test.Count;i++)
+      {
+        Console.WriteLine("Output: " + test[i]);
+      }
+      CollectionAssert.AreEquivalent(output,myWords.AnagramMatch());
+    }
+
+    [TestMethod]
+    public void PartialMatch_MatchHigherThan75_True()
+    {
+      Words myWords = new Words("ball", "bite bool labb llab");
+      string sortedWord = "abht";
+      string sortedListWord = "abhtz";
+      bool matched = myWords.PartialMatch(sortedWord, sortedListWord);
+      Assert.AreEqual(true, matched);
+    }
+
+    [TestMethod]
+    public void AnagramMatch_ListWithPartialMatches_ListOfAnagrams()
+    {
+      Words myWords = new Words("ball", "bite bool labb llab");
+      List <string> output = new List<string> {"labb", "llab"};
+      List<string> test = myWords.AnagramMatch();
+      for(int i=0; i< test.Count;i++)
+      {
+        Console.WriteLine("Output: " + test[i]);
+      }
+      CollectionAssert.AreEquivalent(output, myWords.AnagramMatch());
     }
   }
 }
--- Anagram/Models/Words.cs
+++ Anagram/Models/Words.cs
@@ -5,9 +5,9 @@ namespace AnagramList.Models
 {
   public class Words
   {
-    public string InputWord { get; set; }
-    public string InputString { get; set; }
-    public List<string> InputList = new List<string>();
+    public static string InputWord { get; set; }
+    public static string InputString { get; set; }
+    public static List<string> InputList = new List<string>();
     
     public Words(string str, string stringList)
     {
@@ -18,12 +18,67 @@ namespace AnagramList.Models
 
     public void AddToList()
     {
-      //InputList = InputString.Split(" ", StringSplitOptions.RemoveEmptyEntries);
       string[] newArr = InputString.Split(" ", StringSplitOptions.RemoveEmptyEntries);
       for(int i = 0; i < newArr.Length; i++)
       {
         InputList.Add(newArr[i]);
       }
+    }
+
+    public List<string> AnagramMatch()
+    {
+      List<string> output = new List<string>();
+      string sortedWord = LetterSort(InputWord);
+      for(int i = 0; i < InputList.Count; i++)
+      {
+        string sortedListWord = LetterSort(InputList[i]);
+        if(sortedListWord.Equals(sortedWord))
+        {
+          output.Add(InputList[i]);
+        }
+        else
+        {
+          bool matched = PartialMatch(sortedWord, sortedListWord);
+          if (matched){
+            output.Add(InputList[i]);
+          }
+        }
+      }
+      return output;
+    }
+
+    public bool PartialMatch(string sortedWord, string sortedListWord)
+    {
+      int counter = 0;
+      for(int i = 0; i < sortedWord.Length; i++)
+      {
+        for(int j = 0; j < sortedListWord.Length; j++)
+          if(sortedWord[i] == sortedListWord[j])
+          {
+            counter++;
+            break;
+          }
+      }
+      float matchFraction = (float)counter/(float)sortedListWord.Length;
+      if(matchFraction >= .75){
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    private string LetterSort(string word)
+    {
+      char[] toSort = word.ToCharArray();
+      Array.Sort(toSort);
+      return new string(toSort);
+    }
+
+    public static void ClearAll()
+    {
+      InputWord = "";
+      InputString = "";
+      InputList.Clear();
     }
   }
 }
